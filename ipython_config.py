73437689b77fc5a4e5d1d3f446e47b5255b98758
ewerlopes <ewerlopes@gmail.com>
2017-01-13 12:29:04 +0100

    First Commit

---
+++ ipython_config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Copyright 2016 Ewerton Lopes.
+
+This is licensed under an MIT license. See the LICENSE.txt file
+for more information.
+"""
+
+c.InteractiveShellApp.exec_lines = ['import sys; sys.path.append("./code"); sys.path.append("./data")']
